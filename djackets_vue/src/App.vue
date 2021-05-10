d2d5452fe9404753d86bbfc49fcf08a8d9c98d00
Jlferrete <joseluisferrete@gmail.com>
2021-05-10 19:58:50 +0200

    Improved hamburger menu

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -4,14 +4,14 @@
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item"><strong>Djackets</strong></router-link>
 
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>  
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/winter" class="navbar-item">Winter</router-link>
@@ -39,6 +39,16 @@
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma';
